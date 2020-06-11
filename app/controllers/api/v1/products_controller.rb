b01502a6ca11888d074c43fb81c12b5b740d7dce
Maria Le <hp.mars.le@gmail.com>
2020-06-11 16:52:30 -0500

    add product controller actions

--- app/controllers/api/v1/products_controller.rb
+++ app/controllers/api/v1/products_controller.rb
@@ -1,2 +1,51 @@
-class ProductsController < ApplicationController
+class Api::V1::ProductsController < ApplicationController
+    before_action :set_product, only: [:show, :update, :destroy]
+
+    def index
+        @products = product.all
+    
+        render json: @products
+      end
+    
+      # # GET /products/1
+      def show
+        render json: @product
+      end
+    
+      # POST /products
+      def create
+        @product = product.new(product_params)
+    
+        if @product.save
+          render json: @product, status: :created, location: @product
+        else
+          render json: @product.errors, status: :unprocessable_entity
+        end
+      end
+    
+      # PATCH/PUT /products/1
+      def update
+        if @product.update(product_params)
+          render json: @product
+        else
+          render json: @product.errors, status: :unprocessable_entity
+        end
+      end
+    
+      # DELETE /products/1
+      def destroy
+        @product.destroy
+      end
+    
+      private
+        # Use callbacks to share common setup or constraints between actions.
+        def set_product
+          @product = product.find(params[:id])
+        end
+    
+        # Only allow a trusted parameter "white list" through.
+        def product_params
+          params.require(:product).permit(:name, :image, :ingredients, :description, :size, :price, :brand, :quantity)
+        end
+    end
 end
