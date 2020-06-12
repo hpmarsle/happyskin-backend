080448814b3154642c8c2017629e88304008e820
Maria Le <hp.mars.le@gmail.com>
2020-06-12 14:42:47 -0500

    migrated tables and seed data for each table

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,32 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_192024) do
+ActiveRecord::Schema.define(version: 2020_06_12_030533) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.string "image"
+    t.text "ingredients"
+    t.text "description"
+    t.decimal "price", precision: 7, scale: 2
+    t.string "size"
+    t.string "brand"
+    t.string "quantity"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "star_rating"
+    t.text "comments"
+    t.bigint "product_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["product_id"], name: "index_reviews_on_product_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,43 @@ user = User.create(
     ]
 )
 
+
+product = Product.create(
+    [
+        # {
+        #     name: '',
+        #     image: '',
+        #     ingredients: '',
+        #     description: '',
+        #     price: '',
+        #     brand: '',
+        #     quantity:'',
+        # },
+        {
+            name: 'Hydrating Day & Night Cream',
+            image: 'https://cdn.shopify.com/s/files/1/1736/9637/products/us_tula_24-7moisture_1024x1272_02_1024x.jpg?v=1565878141',
+            ingredients: 'Day & Night Moisturizing Cream Ingredients: Aqua/Water/Eau, Butylene Glycol, Ethylhexyl Palmitate, Squalane, Glycerin, Cetyl Alcohol, Caprylic/Capric Triglyceride, Dimethicone, Hydrogenated Vegetable Oil, Glyceryl Stearate, PEG 100 Stearate, Bifida Ferment Lysate, Lactose, Lactis Proteinum/Milk Protein/Protéine du lait, Yogurt Extract, Hydrolyzed Rice Protein, Cichorium Intybus (Chicory) Root Extract, Vaccinium Angustifolium (Blueberry) Fruit Extract, Olus/Vegetable Oil/Huile végétale, Camelina Sativa Seed Oil, Camellia Sinensis Leaf Extract, Curcuma Longa (Turmeric) Root Extract, Tocopheryl Acetate, Ascorbyl Palmitate, Carthamus Tinctorius (Safflower) Seed Oil, Olea Europaea (Olive) Fruit Oil, Pyrus Malus (Apple) Fruit Extract, Citrullus Lanatus (Watermelon) Fruit Extract, Lens Esculenta (Lentil) Fruit Extract, Sodium Lactate, Sodium PCA, Carbomer, Sodium Hydroxide, Pentylene Glycol, Caprylyl Glycol, Bulnesia Sarmientoi Wood Oil, Citrus Limon (Lemon) Fruit Oil, Citrus Aurantium Dulcis (Orange) Oil, Juniperus Mexicana Oil, Cananga Odorata Flower Oil, Ethylhexylglycerin, Stearyl Alcohol, Disodium EDTA, Citric Acid, Sorbic Acid, Phenethyl Alcohol, Sodium Benzoate, Potassium Sorbate, Phenoxyethanol, Parfum/Fragrance, Alpha-Isomethyl Ionone, Butylphenyl Methylpropional, Linalool, Hexyl Cinnamal.',
+            description: 'Finally, the perfect do-it-all day and night moisturizer. Packed with naturally derived probiotics & superfoods, this nourishing day and night cream delivers deep hydration, firms skin & improves look of fine lines & wrinkles. Skin is left looking more even & glowing.',
+            price: '52.00',
+            brand: 'Tula',
+            size: '1.5 oz / 44g',
+            quantity:'100'
+        }
+        # {
+        #     name: 'Hydrating Day & Night Cream',
+        #     image: 'https://cdn.shopify.com/s/files/1/1736/9637/products/us_tula_24-7moisture_1024x1272_02_1024x.jpg?v=1565878141',
+        #     ingredients: 'Day & Night Moisturizing Cream Ingredients: Aqua/Water/Eau, Butylene Glycol, Ethylhexyl Palmitate, Squalane, Glycerin, Cetyl Alcohol, Caprylic/Capric Triglyceride, Dimethicone, Hydrogenated Vegetable Oil, Glyceryl Stearate, PEG 100 Stearate, Bifida Ferment Lysate, Lactose, Lactis Proteinum/Milk Protein/Protéine du lait, Yogurt Extract, Hydrolyzed Rice Protein, Cichorium Intybus (Chicory) Root Extract, Vaccinium Angustifolium (Blueberry) Fruit Extract, Olus/Vegetable Oil/Huile végétale, Camelina Sativa Seed Oil, Camellia Sinensis Leaf Extract, Curcuma Longa (Turmeric) Root Extract, Tocopheryl Acetate, Ascorbyl Palmitate, Carthamus Tinctorius (Safflower) Seed Oil, Olea Europaea (Olive) Fruit Oil, Pyrus Malus (Apple) Fruit Extract, Citrullus Lanatus (Watermelon) Fruit Extract, Lens Esculenta (Lentil) Fruit Extract, Sodium Lactate, Sodium PCA, Carbomer, Sodium Hydroxide, Pentylene Glycol, Caprylyl Glycol, Bulnesia Sarmientoi Wood Oil, Citrus Limon (Lemon) Fruit Oil, Citrus Aurantium Dulcis (Orange) Oil, Juniperus Mexicana Oil, Cananga Odorata Flower Oil, Ethylhexylglycerin, Stearyl Alcohol, Disodium EDTA, Citric Acid, Sorbic Acid, Phenethyl Alcohol, Sodium Benzoate, Potassium Sorbate, Phenoxyethanol, Parfum/Fragrance, Alpha-Isomethyl Ionone, Butylphenyl Methylpropional, Linalool, Hexyl Cinnamal.',
+        #     description: 'Finally, the perfect do-it-all day and night moisturizer. Packed with naturally derived probiotics & superfoods, this nourishing day and night cream delivers deep hydration, firms skin & improves look of fine lines & wrinkles. Skin is left looking more even & glowing.',
+        #     price: '52.00',
+        #     brand: 'Tula',
+        #     size: '1.5 oz / 44g',
+        #     quantity:'100'
+        # }
+    ]
+)
+
+review = Review.create(
+    [
+        {star_rating: "5 stars", comments: "This is the best product ever. Miracle cream in a jar. How did I live without this all my life. This made me into a 20 year old again and I am 80! Can't have enough.", product_id: 1}
+    ]
+)
