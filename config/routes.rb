09f8379b255a31009b03f0421f80dd86a5b1748e
Maria Le <hp.mars.le@gmail.com>
2020-06-11 22:40:13 -0500

    genrate resource for review model

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  resources :reviews
   namespace :api do
     namespace :v1 do
       resources :users
       resources :products
+      resources :reviews
     end
   end
 
