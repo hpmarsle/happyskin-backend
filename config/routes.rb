c9a31c49f6dbeb7d55d06c8967c796b6304d2949
Maria Le <hp.mars.le@gmail.com>
2020-07-06 14:51:23 -0500

    nest reviews under products routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :users
-      resources :products
-      resources :reviews
+      resources :products do
+        resources :reviews
+      end
     end
   end
 
