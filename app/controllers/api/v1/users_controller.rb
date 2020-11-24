4bbe7f28c36a126fea9f492c4c9b2e6d78b9f615
Maria Le <hp.mars.le@gmail.com>
2020-11-24 15:25:07 -0600

    Adjust route actions and edit user_params to contain password and password confirmation

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,4 +1,14 @@
 class Api::V1::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    session[:user_id] = @user.id
+    
+    if @user.save
+      render json: @user, serializer: UserSerializer, status: :created
+    else 
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end 
 
   def index
     @users = User.all
@@ -6,4 +16,9 @@ class Api::V1::UsersController < ApplicationController
     render json: @users
   end
 
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :username, :password, :password_confirmation)
+  end
 end
