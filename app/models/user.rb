e837737d29d08e78c9e5516534bcc73a24885fc0
Maria Le <hp.mars.le@gmail.com>
2020-12-02 21:55:13 -0600

    establish relationship between user and review

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,5 @@ class User < ApplicationRecord
     has_secure_password
     validates :name, :email, :username, presence: true
     validates :email, :username, uniqueness: true
+    has_many :reviews
 end
